6c961f0a10d606218596b6eba5f337e1b1c7fe0b
takahiro.tominaga <tt.300607@gmail.com>
2022-02-27 01:47:42 +0900

    add: category

--- youtube_ui/lib/screens/home.dart
+++ youtube_ui/lib/screens/home.dart
@@ -13,6 +13,16 @@ class _HomeState extends State<Home> {
     Icons.search,
     Icons.account_circle,
   ];
+  List<String> categories = [
+    "All",
+    "Stoves",
+    "Baking",
+    "Movies",
+    "Flutter",
+    "React Native",
+    "Coding is Love",
+  ];
+  String selectedCategory = "All";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +36,66 @@ class _HomeState extends State<Home> {
             .map((iconName) =>
                 IconButton(onPressed: (){}, icon: Icon(iconName)))
             .toList(),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(50.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Row(
+                  children: [
+                    TextButton(
+                      onPressed: (){},
+                      child: Row(children: [
+                        const Icon(
+                          Icons.explore_outlined, 
+                          color: Colors.black
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Text(
+                            "Explore",
+                            style: Theme.of(context).textTheme.subtitle2,
+                          ),
+                        )
+                      ]),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.grey.shade200),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: SizedBox(
+                        height: 30,
+                        child: VerticalDivider(
+                          thickness: 1,
+                          color: Colors.grey.shade200,
+                        ),
+                      ),
+                    ),
+                    Wrap(
+                      spacing: 5.0,
+                      children: categories
+                        .map((category) => FilterChip(
+                          showCheckmark: false,
+                          selectedColor: Colors.grey.shade600,
+                          label: Text(
+                            category,
+                            style: TextStyle(
+                              color: selectedCategory == category
+                                  ? Colors.white
+                                  : Colors.black
+                            ),
+                          ),
+                          backgroundColor: Colors.grey.shade200,
+                          selected: selectedCategory == category,
+                          onSelected: (bool value) {
+                            setState(() {
+                              selectedCategory = category;
+                            });
+                          }))
+                        .toList())
+                  ],
+                ),
+              )),
       ),
     );
   }

bf51321135ecf52a0d5bf67ad30dbb09ecd3ae97
takahiro.tominaga <tt.300607@gmail.com>
2022-11-20 00:38:21 +0900

    custom color style

--- ticket_booking/lib/main.dart
+++ ticket_booking/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_booking/screens/bottom_bar.dart';
+import 'package:ticket_booking/utils/app_styles.dart';
 
 void main() {
   // Entry point
@@ -13,9 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: primary,
       ),
       home: const BottomBar(),
     );

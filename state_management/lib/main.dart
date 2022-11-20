fbe9414a4fa09d08238ceccb436eb0eabaf5cb2f
takahiro.tominaga <tt.300607@gmail.com>
2022-11-20 10:22:46 +0900

    load json data

---
+++ state_management/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:state_management/blocks/workouts_cubit.dart';
+
+import 'model/workout.dart';
+
+void main() => runApp(const WorkoutTime());
+
+class WorkoutTime extends StatelessWidget {
+  const WorkoutTime({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Workouts',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(
+            color: Color.fromARGB(255, 66, 74, 96),
+          ),
+        ),
+      ),
+      home: BlocProvider<WorkoutsCubit>(
+        create: (BuildContext context) {
+          WorkoutsCubit workoutsCubit = WorkoutsCubit();
+          if (workoutsCubit.state.isEmpty) {
+            print('Loading json ...');
+            workoutsCubit.getWorkouts();
+          }
+          return workoutsCubit;
+        },
+        child: BlocBuilder<WorkoutsCubit, List<Workout>>(
+          builder: (context, state) {
+            return Center(child: const Text("hi there"));
+          },
+        ),
+      ),
+    );
+  }
+}
